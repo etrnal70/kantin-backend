9cbdcc8fb231a24de1d682198be43e0637232005
Mochammad Hanif R <etrnal70@gmail.com>
2022-01-09 06:13:19 +0700

    Define states

---
+++ pkg/constants/state/category.go
@@ -0,0 +1,8 @@
+package state
+
+const (
+	CategoryFood     = 1
+	CategorySnack    = 2
+	CategoryBeverage = 3
+	CategoryDessert  = 4
+)
---
+++ pkg/constants/state/status.go
@@ -0,0 +1,10 @@
+package state
+
+const (
+	OrderWaitingApproval  = 1
+	OrderProcessing       = 2
+	OrderRequestForCancel = 3
+	OrderReady            = 4
+	OrderDone             = 5
+	OrderCancelled        = 6
+)
